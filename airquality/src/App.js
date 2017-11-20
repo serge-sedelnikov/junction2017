ad25a1936f6df42b82947a0db108e04392bed2ee
Sedelnikov <sergey.sedelnikov@storaenso.com>
2017-11-20 21:36:13 +0200

    example app

--- airquality/src/App.js
+++ airquality/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: undefined
+    }
+  }
+
+  /**
+   * On component did mount
+   */
+  componentDidMount() {
+    let url = 'https://woodcityapiqa.azurewebsites.net/api/v1/GraphQL';
+    let query = {
+      query: "{ building(id:\"Junction2017\") { airquality { co2, timestamp }}}"
+    }
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(query)
+    })
+      .then(res => res.json())
+      .then(data => this.setState({
+        data
+      }))
+  }
+
   render() {
+
+    if (!this.state.data) {
+      return 'loading...';
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <pre>
+          <code>{JSON.stringify(this.state.data, null, 2)}</code>
+        </pre>
       </div>
     );
   }
